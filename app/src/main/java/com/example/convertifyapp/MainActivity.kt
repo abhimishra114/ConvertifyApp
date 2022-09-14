fd935b626356818be5562136e5b8eedba3dbe604
Abhisekh <abhisekhmishra26@gmail.com>
2022-09-14 11:54:39 +0530

    created a few funtions int the Main.kt file

--- app/src/main/java/com/example/convertifyapp/MainActivity.kt
+++ app/src/main/java/com/example/convertifyapp/MainActivity.kt
@@ -7,5 +7,29 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+    }
+    // This function checks the string is in Hexadecimal form or not
+    fun isHex(string:String): Boolean{
+        val n = string.length
+        for(i in 0 until n){
+            val ch = string[i]
+            if ((ch < '0' || ch > '9')
+                && (ch < 'A' || ch > 'F')){
+                return false
+            }
+        }
+        return true
+    }
+    // This function checks if the string is in binary form or not
+    fun isBinary(string: String): Boolean{
+        val n = string.length
+        for (i in 0 until n){
+            val ch = string[i]
+            if(ch != '0' || ch != '1'){
+                return false
+            }
+        }
+        return true
     }
 }
